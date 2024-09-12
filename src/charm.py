b2581a43223c0783e45268b0965fae2d97f747e3
James Garner <james.garner@canonical.com>
2024-09-12 21:00:27 +1200

    Add storage and the ability to pass your Calibre Library as a resource
    Also package the default starter calibre library to use as the default
    for calibre-web if the user doesn't supply anything

--- src/charm.py
+++ src/charm.py
@@ -13,6 +13,7 @@ https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
 """
 
 import logging
+from pathlib import Path
 from typing import cast
 
 import ops
@@ -23,6 +24,7 @@ logger = logging.getLogger(__name__)
 VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
 
 CALIBRE_WEB_CONTAINER = 'calibre-web'
+STORAGE_NAME = 'books'
 
 
 class CalibreWebCharmCharm(ops.CharmBase):
@@ -30,16 +32,12 @@ class CalibreWebCharmCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        framework.observe(
-            self.on[CALIBRE_WEB_CONTAINER].pebble_ready, self._on_calibre_web_pebble_ready
-        )
-        #framework.observe(
-        #    self.on[CALIBRE_CONTAINER].pebble_ready, self._on_calibre_pebble_ready
-        #)
+        framework.observe(self.on[CALIBRE_WEB_CONTAINER].pebble_ready, self._on_pebble_ready)
+        framework.observe(self.on[STORAGE_NAME].storage_attached, self._on_storage_attached)
         self.unit.set_ports(8083)
         #framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_calibre_web_pebble_ready(self, event: ops.PebbleReadyEvent):
+    def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Define and start a workload using the Pebble API.
 
         Change this example to suit your needs. You'll need to specify the right entrypoint and
@@ -56,6 +54,78 @@ class CalibreWebCharmCharm(ops.CharmBase):
         # Learn more about statuses in the SDK docs:
         # https://juju.is/docs/sdk/constructs#heading--statuses
         self.unit.status = ops.ActiveStatus()
+
+    def _on_storage_attached(self, event: ops.StorageAttachedEvent):
+        def push_and_extract_library(container: ops.Container, library: bytes):
+            logger.debug("_on_storage_attached: push_and_extract_library")
+            logger.debug("push_and_extract_library: installing dependencies")
+            container.exec(['apt', 'update']).wait()
+            container.exec(['apt', 'install', 'dtrx', '-y']).wait()
+            logger.debug("push_and_extract_library: copying library")
+            container.push('/books/library.zip', library)
+            container.exec(['dtrx', '--noninteractive', 'library.zip'], working_dir='/books').wait()
+            container.exec(['bash', '-c', 'mv /books/library/* /books/']).wait()
+            container.exec(['rm', '/books/library.zip']).wait()
+            container.exec(['rmdir',  '/books/library']).wait()
+            logger.debug("push_and_extract_library: done")
+
+        logger.debug("_on_storage_attached")
+        container = self.framework.model.unit.containers[CALIBRE_WEB_CONTAINER]
+        logger.debug(f"_on_storage_attached: {container=}")
+        library_path = Path(self.framework.model.resources.fetch('calibre-library'))
+        library = library_path.read_bytes()
+        if len(library):  # user provided library
+            logger.debug(f"_on_storage_attached: {library_path=}")
+            push_and_extract_library(container, library)
+        else:  # default library
+            library_path = Path('.') / 'library.zip'
+            logger.debug(f"_on_storage_attached: {library_path=}")
+            library = library_path.read_bytes()
+            assert library
+            push_and_extract_library(container, library)
+        if container.exists('/books/Calibre Library'):  # as a result of a push_and_extract
+            logger.debug("_on_storage_attached: moving /books/Calibre Library contents to /books")
+            container.exec(
+                ['bash', '-c', 'mv --force ./* ./.[!.]* /books/'],
+                working_dir='/books/Calibre Library',
+            )
+            container.exec(['rmdir', '/books/Calibre Library'])
+
+            #class FakeOut:
+            #    def __init__(self):
+            #        self.lines = []
+            #    def write(self, stuff):
+            #        self.lines.append(stuff)
+            #f = FakeOut()
+            #try:
+            #    logger.debug("moving all files to books, this normally fails")
+            #    proc = container.exec(
+            #        ['bash', '-c', 'mv --force ./* ./.[!.]* /books/'],
+            #        working_dir='/books/Calibre Library',
+            #        combine_stderr=True,
+            #        stderr=None,
+            #        stdout=f,
+            #    )
+            #    proc.wait()
+            #except ops.pebble.ExecError as e:
+            #    logger.debug("exception")
+            #    logger.debug(e)
+            #    logger.debug("it works tho ...")
+            #    logger.debug(f.lines)
+            #else:
+            #    logger.debug("no exception")
+            #f2 = FakeOut()
+            #try:
+            #    container.exec(
+            #        ['rmdir', '/books/Calibre Library'],
+            #        combine_stderr=True,
+            #        stderr=None,
+            #        stdout=f2,
+            #    ).wait()
+            #except ops.pebble.ExecError as e:
+            #    logger.debug("exception")
+            #    logger.debug(e)
+            #    logger.debug(f2.lines)
 
     #def _on_config_changed(self, event: ops.ConfigChangedEvent):
     #    """Handle changed configuration.
