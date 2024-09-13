6a9bb8ba4186070ffa76b0d0f488dce8504314f4
James Garner <james.garner@canonical.com>
2024-09-13 12:30:09 +1200

    Support configurable options for handling existing library

--- src/charm.py
+++ src/charm.py
@@ -22,11 +22,12 @@ import ops
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
-VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
-
-CALIBRE_WEB_CONTAINER = 'calibre-web'
+CONTAINER_NAME = 'calibre-web'
 STORAGE_NAME = 'books'
+LIBRARY_WRITE_BEHAVIOUR_KEY = 'library-write'
 
+LibraryWriteBehaviour: typing.TypeAlias = typing.Literal["skip", "clean"]  #, "overwrite"]
+LIBRARY_WRITE_BEHAVIOURS = typing.get_args(LibraryWriteBehaviour)
 
 class CaptureStdOut:
     """For debugging."""
@@ -43,10 +44,11 @@ class CalibreWebCharmCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
-        framework.observe(self.on[CALIBRE_WEB_CONTAINER].pebble_ready, self._on_pebble_ready)
+        framework.observe(self.on[CONTAINER_NAME].pebble_ready, self._on_pebble_ready)
         framework.observe(self.on[STORAGE_NAME].storage_attached, self._on_storage_attached)
+        framework.observe(self.on.config_changed, self._on_config_changed)
+        framework.observe(self.on.collect_unit_status, self._on_collect_status)
         self.unit.set_ports(8083)
-        #framework.observe(self.on.config_changed, self._on_config_changed)
 
     def _on_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
         """Define and start a workload using the Pebble API."""
@@ -59,7 +61,8 @@ class CalibreWebCharmCharm(ops.CharmBase):
     def _pebble_layer(self) -> ops.pebble.LayerDict:
         """Return a dictionary representing a Pebble layer."""
         c = " && ".join([
-            "bash /etc/s6-overlay/s6-rc.d/init-calibre-web-config/run",  # with bash because the shebang depends on s6
+            "bash /etc/s6-overlay/s6-rc.d/init-calibre-web-config/run",
+            # with bash because the run script shebang depends on s6
             "export CALIBRE_DBPATH=/config",
             "cd /app/calibre-web",
             "python3 /app/calibre-web/cps.py",
@@ -85,20 +88,40 @@ class CalibreWebCharmCharm(ops.CharmBase):
         }
 
     def _on_storage_attached(self, event: ops.StorageAttachedEvent):
+        self._push_library_to_storage()
+
+    def _push_library_to_storage(self) -> None:
         """Push user provided or default calibre-library resource to storage."""
-        container = self.framework.model.unit.containers[CALIBRE_WEB_CONTAINER]
-        logger.debug(f"_on_storage_attached: {container=}")
+        try:
+            library_write_behaviour = self._library_write_behaviour
+        except ValueError:
+            return
+        container = self.framework.model.unit.containers[CONTAINER_NAME]
+        if (contents := container.list_files('/books/')):
+            msg_prefix = f"_push_library_to_storage: {library_write_behaviour=}: "
+            match library_write_behaviour:
+                case "skip":
+                    logger.debug(msg_prefix + "returning")
+                    return
+                case "clean":
+                    logger.debug(msg_prefix + "cleaning ...")
+                    for fileinfo in contents:
+                        container.remove_path(fileinfo.path, recursive=True)
+                #case "overwrite":
+                #    logger.debug(msg_prefix + "continuing ...")
+        logger.debug(f"_push_library_to_storage: {container=}")
         library_path = Path(self.framework.model.resources.fetch('calibre-library'))
         library = library_path.read_bytes()
         if len(library):  # user provided library
-            logger.debug(f"_on_storage_attached: {library_path=}")
+            logger.debug(f"_push_library_to_storage: {library_path=}")
             self._push_and_extract_library(container, library)
-        else:  # use default library
-            library_path = Path('.') / 'library.zip'
-            logger.debug(f"_on_storage_attached: {library_path=}")
-            library = library_path.read_bytes()
-            assert library
-            self._push_and_extract_library(container, library)
+            return
+        #else: use default library
+        library_path = Path('.') / 'library.zip'
+        library = library_path.read_bytes()
+        assert library
+        logger.debug(f"_push_library_to_storage: {library_path=}")
+        self._push_and_extract_library(container, library)
 
     def _push_and_extract_library(self, container: ops.Container, library: bytes) -> None:
         logger.debug("_push_and_extract_library: installing dependencies")
@@ -116,21 +139,23 @@ class CalibreWebCharmCharm(ops.CharmBase):
             ['dtrx', '--noninteractive', '--overwrite', 'library.zip'],
             working_dir='/books',
         ).wait()
-        # dtrx always extracts into a folder named after the archive
+
         logger.debug("_push_and_extract_library: flattening /books/library/ to /books/")
+        # dtrx always extracts into a folder named after the archive
         move_contents_up_one_level = [
             'bash',
             '-c',
             (
                 'shopt -s nullglob ; '
                 'mv --force --target-directory=../ ./* ./.[!.]* '
-                '|| true'  # ignore no matches in glob
-            )
+                '|| true'  # ignore no matches in glob ... in addition to other errors >:(
+            ),
         ]
         container.exec(move_contents_up_one_level, working_dir='/books/library').wait()
-        #combine_stderr=True, stderr=None, stdout=cast(typing.BinaryIO, (stdout := CaptureStdOut())),
-        container.exec(['rmdir',  '/books/library']).wait()
-        container.exec(['rm', '/books/library.zip']).wait()
+        #extra exec kwargs when debugging:
+        #kwargs = dict(combine_stderr=True, stderr=None, stdout=cast(typing.BinaryIO, (stdout := CaptureStdOut())))
+        container.exec(['rmdir',  '/books/library']).wait()  # error if not empty
+        container.remove_path('/books/library.zip')
 
         # library.zip may contain the library contents directly, or inside a 'Calibre Library' directory
         if container.exists('/books/Calibre Library'):
@@ -138,70 +163,31 @@ class CalibreWebCharmCharm(ops.CharmBase):
                 "_push_and_extract_library: flattening /books/Calibre Library/ contents to /books"
             )
             container.exec(move_contents_up_one_level, working_dir='/books/Calibre Library').wait()
-            container.exec(['rmdir', '/books/Calibre Library']).wait()
+            container.exec(['rmdir', '/books/Calibre Library']).wait()  # error if not empty
         logger.debug("push_and_extract_library: done")
 
-            #f = FakeOut()
-            #try:
-            #    logger.debug("moving all files to books, this normally fails")
-            #    proc = container.exec(
-            #        ['bash', '-c', 'mv --force ./* ./.[!.]* /books/'],
-            #        working_dir='/books/Calibre Library',
-            #        combine_stderr=True,
-            #        stderr=None,
-            #        stdout=f,
-            #    )
-            #    proc.wait()
-            #except ops.pebble.ExecError as e:
-            #    logger.debug("exception")
-            #    logger.debug(e)
-            #    logger.debug("it works tho ...")
-            #    logger.debug(f.lines)
-            #else:
-            #    logger.debug("no exception")
-            #f2 = FakeOut()
-            #try:
-            #    container.exec(
-            #        ['rmdir', '/books/Calibre Library'],
-            #        combine_stderr=True,
-            #        stderr=None,
-            #        stdout=f2,
-            #    ).wait()
-            #except ops.pebble.ExecError as e:
-            #    logger.debug("exception")
-            #    logger.debug(e)
-            #    logger.debug(f2.lines)
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        """Handle changed configuration."""
+        logger.debug("_on_config_changed")
+        self._push_library_to_storage()
 
-    #def _on_config_changed(self, event: ops.ConfigChangedEvent):
-    #    """Handle changed configuration.
+    @property
+    def _library_write_behaviour(self) -> LibraryWriteBehaviour:
+        library_write_behaviour = cast(str, self.model.config[LIBRARY_WRITE_BEHAVIOUR_KEY]).lower()
+        if library_write_behaviour not in LIBRARY_WRITE_BEHAVIOURS:
+            msg = f"invalid {LIBRARY_WRITE_BEHAVIOUR_KEY}: '{library_write_behaviour}'"
+            self._add_status(ops.BlockedStatus(msg))
+            raise ValueError(msg)
+        return cast(LibraryWriteBehaviour, library_write_behaviour)
 
-    #    Change this example to suit your needs. If you don't need to handle config, you can remove
-    #    this method.
+    def _on_collect_status(self, event: ops.CollectStatusEvent) -> None:
+        event.add_status(ops.ActiveStatus())
 
-    #    Learn more about config at https://juju.is/docs/sdk/config
-    #    """
-    #    # Fetch the new config value
-    #    log_level = cast(str, self.model.config["log-level"]).lower()
-
-    #    # Do some validation of the configuration option
-    #    if log_level in VALID_LOG_LEVELS:
-    #        # The config is good, so update the configuration of the workload
-    #        container = self.unit.get_container("httpbin")
-    #        # Verify that we can connect to the Pebble API in the workload container
-    #        if container.can_connect():
-    #            # Push an updated layer with the new config
-    #            container.add_layer("httpbin", self._pebble_layer, combine=True)
-    #            container.replan()
-
-    #            logger.debug("Log level for gunicorn changed to '%s'", log_level)
-    #            self.unit.status = ops.ActiveStatus()
-    #        else:
-    #            # We were unable to connect to the Pebble API, so we defer this event
-    #            event.defer()
-    #            self.unit.status = ops.WaitingStatus("waiting for Pebble API")
-    #    else:
-    #        # In this case, the config option is bad, so block the charm and notify the operator.
-    #        self.unit.status = ops.BlockedStatus("invalid log level: '{log_level}'")
+    def _add_status(self, status: ops.StatusBase) -> None:
+        """Dirty hack to add status outside _on_collect_status for auto prioritisation."""
+        event = ops.CollectStatusEvent(ops.Handle(parent=None, key=None, kind="whatever"))
+        event.framework = self.framework
+        event.add_status(status)
 
 
 if __name__ == "__main__":  # pragma: nocover
