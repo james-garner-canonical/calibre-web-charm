47310939f0f30f38c76828dfe4ec7e81f9184044
James Garner <james.garner@canonical.com>
2024-09-17 18:38:44 +1200

    Small changes to deploy test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -20,6 +20,7 @@ METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 
 
+@pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     """Build and deploy calibre-web charm, attaching resources and storage.
@@ -35,4 +36,4 @@ async def test_build_and_deploy(ops_test: OpsTest):
         "calibre-library": "./empty.zip",
     }
     await model.deploy(charm, resources=resources, application_name=APP_NAME)
-    await model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000)
+    await model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=600)
