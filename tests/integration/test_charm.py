7ef7e3ba5f92a572e0ab6a07855160e891836f62
James Garner <james.garner@canonical.com>
2024-09-17 16:41:23 +1200

    Add build and deploy smoke test
    Passes in VM. Fails sometimes on local due to juju storage attaching
    shenanigans.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -21,27 +21,18 @@ APP_NAME = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_build(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
+async def test_build_and_deploy(ops_test: OpsTest):
+    """Build and deploy calibre-web charm, attaching resources and storage.
 
-    Assert on the unit status before any relations/configurations take place.
+    Fails sometimes on my machine due to failure to attach storage, but passes in a fresh VM.
     """
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
+    # deploy and attach storage
+    model = cast("Model", ops_test.model)
     resources = {
         "calibre-web-image": METADATA["resources"]["calibre-web-image"]["upstream-source"],
         "calibre-library": "./empty.zip",
     }
-
-    ## something is wrong with attaching storage I think
-    ## Deploy the charm and wait for active/idle status
-    #model = cast("Model", ops_test.model)
-    #await model.deploy(
-    #    charm,
-    #    resources=resources,
-    #    application_name=APP_NAME,
-    #    #storage=METADATA["storage"],
-    #)
-    #await model.wait_for_idle(
-    #    apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=20_000
-    #)
+    await model.deploy(charm, resources=resources, application_name=APP_NAME)
+    await model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000)
